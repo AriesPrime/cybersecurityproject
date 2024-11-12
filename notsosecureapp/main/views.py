ae80860a78399e190a440e7a3745a971b9aaef4a
Ares Beletski <contact.aresbel@gmail.com>
2024-11-12 16:37:49 +0200

    project

---
+++ notsosecureapp/main/views.py
@@ -0,0 +1,145 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post, Comment
+from django.db.models import Q
+from django.db import connection
+from .forms import CommentForm, PostForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
+from django.http import HttpResponseForbidden
+from django.views.decorators.csrf import csrf_exempt
+
+
+def signup(request):
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+        user = form.save()
+        login(request, user)
+        return redirect('home')
+
+    return render(request, 'signup.html', {'form': form})
+
+
+def signin(request):
+    form = AuthenticationForm(data=request.POST)
+    if form.is_valid():
+        user = form.get_user()
+        login(request, user)
+        return redirect('home')
+
+    return render(request, 'signin.html', {'form': form})
+
+
+def signout(request):
+    logout(request)
+    return redirect('home')
+
+
+def home(request):
+    search_query = request.GET.get('search', '')
+    if search_query:
+        posts = Post.objects.filter(
+            Q(id__icontains=search_query) |
+            Q(title__icontains=search_query) |
+            Q(author__username__icontains=search_query)
+        )
+    else:
+        posts = Post.objects.all().order_by('-created_at')[:50]
+
+    return render(request, 'home.html', {'posts': posts, 'search_query': search_query})
+
+def create_post(request):
+
+    form = PostForm(request.GET)
+    if form.is_valid():
+        post = form.save(commit=False)
+        post.author = request.user
+        post.save()
+        return redirect('post_detail', post_id=post.id)
+
+    return render(request, 'create_post.html', {'form': form})
+
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+
+    comments = post.comments.all()
+    form = CommentForm(request.POST or None)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.author = request.user.username
+        comment.save()
+        return redirect('post_detail', post_id=post.id)
+    return render(request, 'post_detail.html', {'post': post, 'comments': comments, 'form': form})
+
+
+def edit_post(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    
+    # Broken Access Control (A01:2021): any user can edit any post
+    # Check if the user is the author of the post
+    # if post.author != request.user:
+    #    return redirect('home')  # Redirect if not the author
+
+    form = PostForm(request.POST or None, instance=post)
+    if form.is_valid():
+        form.save()
+        return redirect('post_detail', post_id=post.id)
+    return render(request, 'edit_post.html', {'form': form, 'post': post})
+
+
+def search_posts(request):
+    query = request.GET.get('q')
+    cursor = connection.cursor()
+
+    cursor.execute(
+        "SELECT * FROM main_post WHERE title LIKE %s", [f'%{query}%'])
+    posts = cursor.fetchall()
+
+    return render(request, 'search_results.html', {'posts': posts})
+
+
+# Cross-Site Request Forgery (CSRF): app is vulnerable to CSRF attacks
+@csrf_exempt
+def add_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+
+    if request.method == 'POST':
+        Comment.objects.create(
+            post=post, author=request.user, content=request.POST.get('content'))
+        return redirect('post_detail', post_id=post.id)
+    return render(request, 'add_comment.html', {'post': post})
+
+# Fix:
+# Remove @csrf_exempt and use CSRF protection on the form
+# in the HTML template (e.g., {% csrf_token %} in forms)
+
+
+def delete_post(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+
+    # Broken Access Control (A01:2021): any user can delete any post
+    post.delete()
+
+    # Fix:
+    # Ensure only the post author can delete the post
+    # if post.author != request.user.username:
+    #     return HttpResponseForbidden("You are not allowed to delete this post.")
+    # post.delete()
+
+    return redirect('home')
+
+
+def delete_comment(request, comment_id):
+    comment = get_object_or_404(Comment, id=comment_id)
+
+    # Broken Access Control (A01:2021): any user can delete any comment
+    comment.delete()
+
+    # Fix:
+    # Ensure only the comment author can delete the comment
+    # if comment.author != request.user.username:
+    #     return HttpResponseForbidden("You are not allowed to delete this comment.")
+    # comment.delete()
+
+    return redirect('post_detail', post_id=comment.post.id)
---
+++ notsosecureapp/notsosecureapp/urls.py
@@ -0,0 +1,34 @@
+"""
+URL configuration for notsosecureapp project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from main import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('create_post/', views.create_post, name='create_post'),
+    path('edit_post/<int:post_id>/', views.edit_post, name='edit_post'),
+    path('search/', views.search_posts, name='search_posts'),
+    path('add_comment/<int:post_id>/', views.add_comment, name='add_comment'),
+    path('post/<int:post_id>/', views.post_detail, name='post_detail'),
+    path('signup/', views.signup, name='signup'),
+    path('signin/', views.signin, name='signin'),
+    path('signout/', views.signout, name='signout'),
+    path('post/<int:post_id>/delete/', views.delete_post, name='delete_post'),
+    path('comment/<int:comment_id>/delete/', views.delete_comment, name='delete_comment'),
+]
